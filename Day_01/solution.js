8c933dc4b689b32eb3ed66c3156fbb37f75380f9
Viktors Jenovs <jviktors@gmail.com>
2019-12-01 09:12:12 +0200

    Add Day_01 solutions

---
+++ Day_01/solution.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { readFileSync } from 'fs';
+
+const input = readFileSync('./input.txt', 'utf8')
+  .split('\n')
+  .map(Number);
+
+const result1 = input.reduce((acc, mass) => {
+  return acc + Math.floor(mass / 3) - 2;
+}, 0);
+
+/**
+ * @param {Number} n
+ */
+const calcMass = n => {
+  const m = Math.floor(n / 3) - 2;
+
+  return m <= 0 ? 0 : m + calcMass(m);
+};
+
+const result2 = input.reduce((acc, mass) => {
+  return acc + calcMass(mass);
+}, 0);
+
+console.log('Part One:', result1);
+console.log('Part Two:', result2);
